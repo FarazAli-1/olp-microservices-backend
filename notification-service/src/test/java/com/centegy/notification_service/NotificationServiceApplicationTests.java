532089ddb632ab9888b0eaa664598bb00f620b93
FarazAli-1 <real.faraz10@gmail.com>
2026-07-15 12:14:22 +0500

    Initial commit

---
+++ notification-service/src/test/java/com/centegy/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centegy.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
